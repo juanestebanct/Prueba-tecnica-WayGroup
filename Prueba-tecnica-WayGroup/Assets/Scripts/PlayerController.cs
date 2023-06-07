ad3ecf09731a4da89f8b0b22c9d9aabf5a1e28b3
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2023-06-06 23:42:18 -0500

    1. Interacción de Objetos basada en Físicas:
    Se hizo el sistema de Interacción de objetos con un Raycast, que va a revisar los objetos que se puedan agarrar y si presionas la q(aun no se decide si va a hacer la tecla  ) lo agarras y si la vuelves a presionar la sueltas, pero si presionas clic derecho lo vas a tirar con cierta fuerza, entre mas esperes mas duro lo tiras

--- Prueba-tecnica-WayGroup/Assets/Scripts/PlayerController.cs
+++ Prueba-tecnica-WayGroup/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -18,16 +19,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform pointReference;
     [SerializeField] private LayerMask layerGround;
 
+    [Header("Grap sistem ")]
+
+    [SerializeField] private float maxDistance, timePrees, forcelaunch;
+    [SerializeField] private Transform positionObjects;
+    [SerializeField] private GameObject hands, objectsGrabable;
+    [SerializeField] private bool isGrabable, throwable;
+  
     private Rigidbody rb;
     private Transform cameraTransform;
-    public bool isGrounded;
+    private bool isGrounded;
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         isGrounded = true;
 
         cameraTransform = Camera.main.transform;
 
         rb = GetComponent<Rigidbody>();
+        hands.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,7 +48,8 @@ public class PlayerController : MonoBehaviour
     {
         Jump();
         Move();
-       
+        ViewObject();
+      
     }
     #region Private fuctions 
    /// <summary>
@@ -71,6 +85,87 @@ public class PlayerController : MonoBehaviour
     {
         return Physics.Raycast(pointReference.position,Vector3.down, GroundDistance, layerGround);
     }
+    /// <summary>
+    /// en esta funcion se analiza primero con un raycast si collisiona con un objeto en el rango que exites 
+    /// luego si es un objeto agarrable, se puede agarrar este objeto,y si detecta que tienes un objeto no puedes agarra 
+    /// otro objeto, cuando se agarra el objeto este se sujeta y se desactiva la gravedad, el kinetic y se pone
+    /// en una direccion 
+    /// </summary>
+    private void ViewObject()
+    {
+        if (isGrabable)
+        {
+            Debug.Log("se lanzara el objeto");
+            ThrowObject();
+            return;
+        }
+        Debug.DrawRay(cameraTransform.position,cameraTransform.forward *maxDistance, Color.red);
 
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit,maxDistance))
+        {
+            if (hit.transform.tag == "Grabable" && Input.GetKeyDown(KeyCode.Q))
+            {
+                Debug.Log("collisiono" + hit.transform);
+                hands.gameObject.SetActive(true);
+                hit.transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                hit.transform.GetComponent<Rigidbody>().useGravity = false;
+                hit.transform.GetComponent<Rigidbody>().isKinematic = true;
+                hit.transform.parent = positionObjects;
+                hit.transform.position = positionObjects.position;
+
+                objectsGrabable = hit.transform.gameObject;
+                isGrabable =true;
+            }
+            
+        }
+    }
+    /// <summary>
+    /// se detecta y precionando la q lo tiras, cuando precionas el clic izq lo lanzas 
+    /// cuando detecta que lo precionas mas tiempo lo lanza con mucha fuerza 
+    /// </summary>
+    private void ThrowObject()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("se solto ");
+            DisambleObjectGrable();
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            //si anda precionado el boton y lo deja de precionar salta 
+            timePrees += Time.deltaTime;
+            Debug.Log(timePrees);
+            throwable = true;
+        }
+        else if (throwable)
+        {
+            DisambleObjectGrable();
+        }
+        //lanzar 
+    }
+    /// <summary>
+    /// para el funcionamiento correcto del lanzamiento se necesita cuando se lance el objeto
+    /// activar la gravedad y desactivar el kinematic.Ademas tambien se calcula si se quiere 
+    /// que se lance la pelota con fuerza 
+    /// </summary>
+    private void DisambleObjectGrable()
+    {
+        objectsGrabable.transform.GetComponent<Rigidbody>().useGravity = true;
+        objectsGrabable.transform.parent = null;
+        objectsGrabable.transform.GetComponent<Rigidbody>().isKinematic = false;
+        if (throwable)
+        {
+            if(timePrees>2) timePrees = 2;
+            objectsGrabable.transform.GetComponent<Rigidbody>().AddForce(cameraTransform.forward * forcelaunch * timePrees, ForceMode.Impulse);
+            throwable = false;
+            timePrees = 0;
+        }
+        objectsGrabable = null;
+        isGrabable = false;
+        hands.gameObject.SetActive(false);
+    }
     #endregion
 }
